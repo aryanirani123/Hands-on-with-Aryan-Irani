428302c30c25fd9ba2953a8a760c8708d3683482
Aryan Irani <64119688+aryanirani123@users.noreply.github.com>
2022-09-06 12:04:59 +0530

    Create Stacks.py

---
+++ Stacks.py
@@ -0,0 +1,30 @@
+#Stack in python 
+
+"""
+Declaration
+Push 
+Pop 
+Top
+
+"""
+
+stack = []
+
+stack.append("a")
+stack.append("b")
+stack.append("c")
+
+print(stack)
+
+#Pop elements 
+
+stack.pop(0)
+print(stack)
+
+
+web_stack = []
+web_stack.append('https://www.cnn.com/')
+web_stack.append('https://www.cnn.com/world')
+web_stack.append('https://www.cnn.com/india')
+web_stack.append('https://www.cnn.com/china')
+print(web_stack)
